b74d7ed64a1d5d6745882d29948a12c95c5c1963
leenaabbi <abbil@sheridancollege.ca>
2020-02-12 14:25:10 -0500

    3st commit

--- src/Blackjack.java
+++ src/Blackjack.java
@@ -7,10 +7,12 @@ import java.util.Scanner;
  * @author Fujitsu
  */
 public class Blackjack {
+
     Scanner scanner = new Scanner(System.in);
     ArrayList deck = new ArrayList();
     ArrayList playerCards = new ArrayList();
     ArrayList dealerCards = new ArrayList();
+
     public void play() {
         createDeck();
         showDeck();
@@ -22,8 +24,16 @@ public class Blackjack {
 
         showPlayerCard();
         showDealerCard();
-    
-        while(true) {
+
+        if (dealer_sum() >= calcPlayerSum()) {
+            System.out.println("Dealer wins");
+            System.exit(0);
+        } else {
+            System.out.println("Player wins");
+            System.exit(0);
+        }
+
+        while (true) {
             System.out.println("Hit or Stay");
             String choice = scanner.nextLine();
             if (choice.toUpperCase().equals("S")) {
@@ -36,21 +46,20 @@ public class Blackjack {
                 System.exit(0);
             }
         }
-             while (true){
-              if(dealer_sum() < 16){
-               dealToDealer();
-               //showDealerCard();
-              }
-              if(dealer_sum() > 21){
-              System.out.println("Sorry you Busted Game Over");
-             System.exit(0);
-              }
+        while (true) {
+            if (dealer_sum() < 16) {
+                dealToDealer();
+                //showDealerCard();
+            }
+            if (dealer_sum() > 21) {
+                System.out.println("Sorry you Busted Game Over");
+                System.exit(0);
+            }
         }
 
     }
 
-
-    public void createDeck(){
+    public void createDeck() {
         String[] suits = new String[]{"H", "D", "C", "S"};
         for (int j = 0; j < 4; j++) {
             for (int i = 1; i <= 13; i++) {
@@ -60,18 +69,19 @@ public class Blackjack {
                 deck.add(card);
             }
         }
-       //Collections.shuffle.(deck);
+        //Collections.shuffle.(deck);
     }
-    public void showDeck(){
-        for(int i = 0; i < deck.size(); i++) {
+
+    public void showDeck() {
+        for (int i = 0; i < deck.size(); i++) {
             Card c = (Card) deck.get(i);
-            System.out.println( c.suit+ " of "  + c.rank);
+            System.out.println(c.suit + " of " + c.rank);
         }
     }
     //deal two cards to player
 
     public void dealToPlayer() {
-        Card c = (Card)deck.remove(0);
+        Card c = (Card) deck.remove(0);
         playerCards.add(c);
 
     }
@@ -82,6 +92,7 @@ public class Blackjack {
         dealerCards.add(c);
 
     }
+
     public void showPlayerCard() {
         System.out.println("Player cards:");
         for (int i = 0; i < playerCards.size(); i++) {
@@ -89,14 +100,16 @@ public class Blackjack {
             System.out.println(c.rank + " of " + c.suit);
         }
     }
-    public void showDealerCard(){
-         System.out.println("Dealer cards:");
+
+    public void showDealerCard() {
+        System.out.println("Dealer cards:");
         for (int i = 0; i < dealerCards.size(); i++) {
             Card c = (Card) dealerCards.get(i);
             System.out.println(c.rank + " of " + c.suit);
         }
     }
-             int sum = 0;
+    int sum = 0;
+
     public int calcPlayerSum() {
         for (int i = 0; i < playerCards.size(); i++) {
             Card c = (Card) playerCards.get(i);
@@ -109,25 +122,19 @@ public class Blackjack {
         }
         return sum;
     }
-         int dealer_sum=0;
-        public int dealer_sum(){
-        for(int i=0; i < deck.size();i++){
-        Card c =(Card)dealerCards.get(i);
-        if(c.rank > 10){
-         dealer_sum=sum+10;
-        }
-        else{
-           dealer_sum=sum+c.rank;
-        }
-        
+    int dealer_sum = 0;
+
+    public int dealer_sum() {
+        for (int i = 0; i < deck.size(); i++) {
+            Card c = (Card) dealerCards.get(i);
+            if (c.rank > 10) {
+                dealer_sum = sum + 10;
+            } else {
+                dealer_sum = sum + c.rank;
+            }
+
         }
         return dealer_sum;
-        }
-     //  if(dealer_sum() > 21 || <calcPlayerSum()){
-   //         System.out.println("Dealer wins");
-  //}
-     //  else{
-           // System.out.println("You wins");  
-          //  }
-      
+    }
+
 }
